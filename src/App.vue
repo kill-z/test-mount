71235c7135ee363db2b0c4ba9d63c7cd8f35e52f
kill-z <killzisme@gmail.com>
2021-05-12 12:47:29 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div
+      v-for="item in list"
+      :key="item.id"
+      >
+      <Card :name="item.name" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Card from '@/components/Card'
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  data () {
+    return {
+      list: []
+    }
+  },
+  mounted () {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        this.list = res.data
+      })
+    console.log('fetch finished')
+    this.$nextTick(() => {
+      console.log('next tick')
+    })
+  }
+
+}
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    User: {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    name: {
+      type: String,
+      default: 'John Doe'
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    console.log('component mounted')
+  }
+}
+
+</script>
